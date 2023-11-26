0a323d3f34b0183cbd5875012db608b9b342154d
Daniele Megna <daniele.megna@qmates.tech>
2023-11-26 18:34:50 +0100

    Refactor match tests: group tests in describe groups

--- tests/match.spec.ts
+++ tests/match.spec.ts
@@ -4,51 +4,56 @@ import scheduledMatchSample from './api_samples/scheduled_match.json';
 import matchDetailsFullWithMe from './api_samples/match_details/full_with_me.json';
 import matchDetailsFullWithoutMe from './api_samples/match_details/full_without_me.json';
 
-describe('Match', () => {
+describe('build match', () => {
 
-  test('build from scheduled match api object', async () => {
-    const match = matchFrom(scheduledMatchSample)
+  describe('from match details api object', () => {
 
-    expect(match.id).toBe(3004643)
-    expect(match.available_slots).toBe(2)
-    expect(match.my_side).toBe(Side.WHITE)
-    expect(match.starting_at.toISOString()).toBe("2023-09-11T18:00:00.000Z")
+    test('full with me match', async () => {
+      const match = matchFrom(matchDetailsFullWithMe)
+
+      expect(match.id).toBe(3015825)
+      expect(match.available_slots).toBe(0)
+      expect(match.my_side).toBe(Side.BLACK)
+      expect(match.starting_at.toISOString()).toBe("2023-11-28T19:00:00.000Z")
+    })
+
+    test('full without me match', async () => {
+      const match = matchFrom(matchDetailsFullWithoutMe)
+
+      expect(match.id).toBe(3017265)
+      expect(match.available_slots).toBe(0)
+      expect(match.my_side).toBeNull()
+      expect(match.starting_at.toISOString()).toBe("2023-11-30T18:00:00.000Z")
+    })
+
   })
 
-  test('build from match details full with me api object', async () => {
-    const match = matchFrom(matchDetailsFullWithMe)
+  describe('from scheduled match api object', () => {
 
-    expect(match.id).toBe(3015825)
-    expect(match.available_slots).toBe(0)
-    expect(match.my_side).toBe(Side.BLACK)
-    expect(match.starting_at.toISOString()).toBe("2023-11-28T19:00:00.000Z")
-  })
+    test('has properly match fields values', async () => {
+      const match = matchFrom(scheduledMatchSample)
 
-  test('build from match details full without me api object', async () => {
-    const match = matchFrom(matchDetailsFullWithoutMe)
+      expect(match.id).toBe(3004643)
+      expect(match.available_slots).toBe(2)
+      expect(match.my_side).toBe(Side.WHITE)
+      expect(match.starting_at.toISOString()).toBe("2023-09-11T18:00:00.000Z")
+    })
 
-    expect(match.id).toBe(3017265)
-    expect(match.available_slots).toBe(0)
-    expect(match.my_side).toBeNull()
-    expect(match.starting_at.toISOString()).toBe("2023-11-30T18:00:00.000Z")
-  })
-
-  test('properly map my side field', async () => {
-    const onBlackSide = sampleWithSideKey(2)
-
-    const match = matchFrom(onBlackSide)
-
-    expect(match.my_side).toBe(Side.BLACK)
-  })
-
-  function sampleWithSideKey(side_key_value: number): any {
-    return {
-      ...scheduledMatchSample,
-      ref_player: {
-        ...scheduledMatchSample.ref_player,
-        side_key: side_key_value
+    test('has properly map my_side field value', async () => {
+      const onBlackSide = {
+        ...scheduledMatchSample,
+        ref_player: {
+          ...scheduledMatchSample.ref_player,
+          side_key: 2
+        }
       }
-    }
-  }
+
+      const match = matchFrom(onBlackSide)
+
+      expect(match.my_side).toBe(Side.BLACK)
+    })
+
+  })
+
 
 })
