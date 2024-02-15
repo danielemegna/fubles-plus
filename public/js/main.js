3c20bf8362ed0ca77bccfe5830daa2a228c33546
Daniele Megna <daniele.megna@qmates.tech>
2024-02-15 07:54:06 +0100

    Render played matches in page with vanilla javascript

--- public/js/main.js
+++ public/js/main.js
@@ -9,4 +9,43 @@ const fublesApi = new FublesAPI(
 
 const matches = await fublesApi.getMyLastPlayedMatches();
 
-console.log(matches)
+document.querySelector('main').innerHTML = matches.map((matchSummary) => {
+  return `
+    <div class="match-card">
+      <div class="match-card__date">
+        ${matchSummary.starting_at.toLocaleString('it-IT', {
+          weekday: 'long', //  values: 'long', 'short', 'narrow'
+          month: 'long', //  values: 'numeric', '2-digit', 'long', 'short', 'narrow'
+          day: 'numeric', //  values: 'numeric', '2-digit'
+        })}
+      </div>
+      <div class="match-card__body">
+        <div class="match-card__grid">
+          <span class="match-card__sport-type">Calcio a 5 &bull; Coperto</span>
+          <div class="match-card__outcome">
+            <span class="${matchSummary.my_side == 'white' ? 'match-card__self' : ''}">
+              ${matchSummary.points.white}
+            </span>
+            <span>-</span>
+            <span class="${matchSummary.my_side == 'black' ? 'match-card__self' : ''}">
+              ${matchSummary.points.black}
+            </span>
+          </div>
+          <span class="match-card__structure-name">Sport Time Corsico</span>
+          <div class="match-card__average-vote">
+            Voto: <span id="match-avg-vote-0">${matchSummary.avg_received_vote}</span>
+          </div>
+        </div>
+        <div class="match-card__votes">
+          <span>Donato Betulle</span>
+          <span>8</span>
+          <span>Giuseppe Domai</span>
+          <span>7.5</span>
+          <span>Ivan Lama</span>
+          <span>7.5</span>
+          <span>Emilio Ferro</span>
+          <span>7</span>
+        </div>
+      </div>
+    </div>`;
+}).join("");
