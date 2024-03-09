3e71c0f582a54705a12ac2c4d3e1c2085086c86a
Daniele Megna <daniele.megna@qmates.tech>
2024-03-09 10:11:49 +0100

    Converted jest.config.js in jest.config.json
    .. to be compiliant with a package of type "module"
    see https://stackoverflow.com/a/71345603

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'ts-jest',
-    setupFilesAfterEnv: ["jest-extended/all"],
-    testEnvironment: 'node',
-    testPathIgnorePatterns: [".d.ts", ".js"],
-    transform: { '^.+\\.ts?$': 'ts-jest' },
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
-};
