739014e10b22d31e46b49d4c1945e425af64c420
Daniele Megna <daniele.megna@qmates.tech>
2024-07-20 11:49:02 +0200

    Create main http server scaffolding

---
+++ backend/src/WebRequest.ts
@@ -0,0 +1,40 @@
+import { IncomingMessage } from "http"
+
+export type WebRequestMethod = 'GET' | 'POST'
+
+export default class WebRequest {
+  readonly method: WebRequestMethod
+  readonly url: string
+  readonly requestBody: any
+
+  static from(request: IncomingMessage, receivedData: string): WebRequest {
+    const requestBody = this.parseRequestBody(receivedData)
+    const requestMethod = this.parseRequestMethod(request.method)
+    return new WebRequest(requestMethod, request.url!, requestBody)
+  }
+
+  private constructor(method: WebRequestMethod, url: string, requestBody: any) {
+    this.method = method
+    this.url = url
+    this.requestBody = requestBody
+  }
+
+  private static parseRequestBody(receivedData: string): any {
+    if (!receivedData || receivedData == '')
+      return undefined
+
+    try {
+      return JSON.parse(receivedData)
+    } catch (error) {
+      console.log('Invalid not-empty request body', receivedData)
+      return undefined
+    }
+  }
+
+  static parseRequestMethod(method: string | undefined): WebRequestMethod {
+    if(method === 'GET' || method === 'POST')
+      return method
+
+    throw new Error("Cannot recongnize method: " + method)
+  }
+}
