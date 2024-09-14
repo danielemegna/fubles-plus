422f81ce52420621b01798a7de5309abf194ae7e
Daniele Megna <daniele.megna@qmates.tech>
2024-09-14 15:28:13 +0200

    Diversify GetFlashEnrollmentsUsecase fake data

--- backend/src/usecase/GetFlashEnrollmentsUsecase.ts
+++ backend/src/usecase/GetFlashEnrollmentsUsecase.ts
@@ -4,18 +4,25 @@ export default class GetFlashEnrollmentsUsecase {
   run(userId: number): WatchingMatch[] {
     const watchingMatches: WatchingMatch[] = [
       {
-        id: 99998,
-        starting_at: new Date(2024, 6, 22, 20, 0),
+        id: 99997,
         title: "Calcio a 5",
+        starting_at: new Date(2024, 10, 18, 20, 30),
         structure_name: "Sport Time Corsico",
         desired_side: DesiredSide.WHITE,
       },
       {
-        id: 99999,
-        starting_at: new Date(2024, 6, 24, 20, 0),
+        id: 99998,
         title: "Calcio a 5",
-        structure_name: "Sport Time Corsico",
+        starting_at: new Date(2024, 10, 20, 20, 0),
+        structure_name: "Tennis Calcetto Barona (T.C.B.)",
         desired_side: DesiredSide.BLACK,
+      },
+      {
+        id: 99999,
+        title: "Calcio a 5",
+        starting_at: new Date(2024, 10, 26, 19, 30),
+        structure_name: "Sport Time Corsico",
+        desired_side: DesiredSide.ANY,
       }
     ];
     return watchingMatches
