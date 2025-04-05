a265c3ea61258a876f559a934a96ffe35fc94f64
Daniele Megna <megna.dany@gmail.com>
2025-04-05 16:16:51 +0200

    Let generate the user ics calendar file in backend daemon
    fake calendar now, dynamic calendar with real matches later

--- backend/src/daemon.ts
+++ backend/src/daemon.ts
@@ -1,4 +1,6 @@
+import fs from 'fs'
 import { FublesSDK } from 'fubles-plus'
+import ical from 'ical-generator'
 
 const LOOP_INTERVAL_IN_MINUTES = 5
 
@@ -21,4 +23,17 @@ async function writeIcsCalendarFile() {
   const fublesSdk = new FublesSDK(authenticatedUser)
   const matches = await fublesSdk.getMyNextScheduledMatches()
   console.debug('next matches:', matches)
+
+  const calendar = ical({ name: "Daniele's Fubles Matches Calendar" })
+  calendar.createEvent({
+    start: new Date(2025, 3, 18, 19, 30),
+    end: new Date(2025, 3, 18, 20, 30),
+    summary: 'Calcetto a 5',
+    description: '',
+    location: 'Sport Time Corsico',
+    url: 'https://app.fubles.com/it/app/matches/3083151'
+  })
+
+
+  fs.writeFileSync(`./calendars/${authenticatedUser.id}.ics`, calendar.toString())
 }
