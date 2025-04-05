7adb73248f71af90fef22430b268c72cc593cf85
Daniele Megna <megna.dany@gmail.com>
2025-04-05 12:55:53 +0200

    Add fubles-plus as local dependency in backend project

--- backend/src/daemon.ts
+++ backend/src/daemon.ts
@@ -1,10 +1,24 @@
+import { FublesSDK } from 'fubles-plus'
+
 const LOOP_INTERVAL_IN_MINUTES = 5
 
-export default function start() {
-  loopHit()
+export default async function start() {
+  await loopHit()
   setInterval(loopHit, LOOP_INTERVAL_IN_MINUTES * 60 * 1000)
 }
 
-function loopHit() {
+async function loopHit() {
   console.log(`Deamon loop hit (next in ${LOOP_INTERVAL_IN_MINUTES} minutes)...`)
+  await writeIcsCalendarFile()
+}
+
+async function writeIcsCalendarFile() {
+  const authenticatedUser = {
+    id: 55576,
+    bearerToken: "<Bearer Token Here>"
+  }
+
+  const fublesSdk = new FublesSDK(authenticatedUser)
+  const matches = await fublesSdk.getMyNextScheduledMatches()
+  console.debug('next matches:', matches)
 }
