0a9458ba7fb3c02eb5fe8ebe0bcbd1aadfdcde65
Daniele Megna <megna.dany@gmail.com>
2025-04-06 19:34:29 +0200

    Export MatchSummary type from sdk

--- sdk/src/exports.ts
+++ sdk/src/exports.ts
@@ -1,4 +1,6 @@
 import FublesSDK from "./fubles_sdk";
 import FublesPlusSDK from "./fubles_plus_sdk";
+import { MatchSummary } from "./match";
 
 export { FublesSDK, FublesPlusSDK }
+export type { MatchSummary }
