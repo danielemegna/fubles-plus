e5dc42c6903319e1030f1512fab71b4250fc5b1d
Daniele Megna <megna.dany@gmail.com>
2025-04-06 19:38:40 +0200

    Use FublesSDK to fetch next matches and generate the calendar
    ... in GenerateUserIcsCalendarUseCase

--- backend/src/usecase/GenerateUserIcsCalendarUseCase.ts
+++ backend/src/usecase/GenerateUserIcsCalendarUseCase.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
-import ical from 'ical-generator'
-import { FublesSDK } from "fubles-plus-sdk"
+import ical, { ICalEventData } from 'ical-generator'
+import { FublesSDK, MatchSummary } from "fubles-plus-sdk"
 
 export default class GenerateUserIcsCalendarUseCase {
   private fublesSDK: FublesSDK
@@ -10,20 +10,35 @@ export default class GenerateUserIcsCalendarUseCase {
   }
 
   async run(userId: number): Promise<void> {
-    //const matches = await this.fublesSDK.getMyNextScheduledMatches()
-    //console.debug('next matches:', matches)
+    const matches = await this.fublesSDK.getMyNextScheduledMatches()
+    console.debug('next matches:', matches)
 
     const calendar = ical({ name: "Daniele's Fubles Matches Calendar" })
-    calendar.createEvent({
-      start: new Date(2025, 3, 18, 19, 30),
-      end: new Date(2025, 3, 18, 20, 30),
-      summary: 'Calcetto a 5',
-      description: '',
-      location: 'Sport Time Corsico',
-      url: 'https://app.fubles.com/it/app/matches/3083151'
-    })
+
+    for (const match of matches) {
+      const event: ICalEventData = this.matchToICalEvent(match)
+      calendar.createEvent(event)
+    }
 
     fs.writeFileSync(`./calendars/${userId}.ics`, calendar.toString())
   }
 
+  private matchToICalEvent(match: MatchSummary): ICalEventData {
+    const endDate = new Date(match.starting_at.getTime())
+    endDate.setHours(match.starting_at.getHours() + 1)
+    const description = 'Metti la maglia ' + (match.my_side == 'black' ? 'NERA' : 'BIANCA') + '!'
+
+    // TODO to create a detailed match we should use MatchDetails
+    // but it is available only with separated api call using
+    // this.fublesSDK.matchDetails(matchId)
+
+    return {
+      start: match.starting_at,
+      end: endDate,
+      summary: 'Calcetto a 5',
+      description: description,
+      location: 'Sport Time Corsico',
+      url: 'https://app.fubles.com/it/app/matches/' + match.id
+    }
+  }
 }
